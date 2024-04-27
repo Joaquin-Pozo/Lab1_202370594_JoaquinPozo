b1deda361a61066d797036455362e84c2309510f
Joaquin Pozo <joaquin.pozo@usach.cl>
2024-04-27 17:24:43 -0400

    Req 6 y 8 corregidos con recursión de cola

--- TDA_line.rkt
+++ TDA_line.rkt
@@ -1,7 +1,11 @@
 #lang scheme
-(provide get-line-sections)
+(provide get-line-sections get-first-line-section)
 
 ;; TDA: SELECTOR - Funcion adicional que permite obtener todas las secciones de una linea
 ;; DOM: line (line)
 ;; REC: sections (sections)
 (define get-line-sections (lambda (line) (last line)))
+;; TDA: SELECTOR - Funcion adicional que permite obtener la primera sección de una linea
+;; DOM: sections (list)
+;; REC: section (section)
+(define get-first-line-section (lambda (line-sections) (first line-sections)))
--- main.rkt
+++ main.rkt
@@ -7,59 +7,26 @@
 ;: Req 2: Funcion constructora de estaciones de metro - TDA Constructor
 ;: DOM: id (int) X nombre (str) X tipo (str) X tiempo-parada (int)
 ;: REC: station (list)
+(define t 0) ; estacion terminal
+(define c 1) ; estacion de combinacion
+(define r 2) ; estacion regular
+(define m 3) ; estacion de mantencion
 
-(define t "terminal")
-(define c "combinacion")
-(define r "regular")
-(define m "mantencion")
 (define station
   (lambda (id name type stop-time)
     (list id name type stop-time)))
-#|
-;creando una nueva estación
-(define e0 (station 1 "USACH" c 30))
-(define e1 (station 2 "Estación Central" c 45))
-(define e2 (station 3 "ULA" r 45))
-(define e3 (station 4 "República" r 45))
-(define e4 (station 5 "Los Héroes" c 60))
-(define e5 (station 6 "Toesca" r 40))
-(define e6 (station 7 "La Moneda" r 40))
-(define e7 (station 8 "Cochera" m 3600))
-(define e8 (station 9 "Parque OHiggins" r 30))
-(define e9 (station 10 "San Pablo" t 40))
-(define e10 (station 11 "Los Dominicos" t 60))
-;e0 e1 e2 e3 e4 e5 e6 e7 e8 e9 e10
-|#
 ;: Req 3: Funcion constructora de enlaces entre dos estaciones de metro - TDA Constructor
 ;: DOM: point1 (station) X point2 (station) X distance (positive-number) X cost (positive-number U {0}). 
 ;: REC: section (list)
-
 (define section
   (lambda (point1 point2 distance cost)
     (list point1 point2 distance cost)))
-#|
-;creando una nueva sección
-(define s0 (section e0 e1 2 50))
-(define s1 (section e1 e2 2.5 55))
-(define s2 (section e2 e3 1.5 30))
-(define s3 (section e3 e4 3 45))
-(define s4 (section e4 e5 3 45))
-(define s5 (section e4 e6 1.4 50))
-(define s6 (section e5 e8 2 40))
-(define s7 (section e0 e7 3 0))
-(define s8 (section e0 e9 7 100))
-(define s9 (section e6 e10 15 250))
-|#
 ;: Req 4: Funcion constructora de una linea de metro - TDA Constructor
 ;: DOM: id (int) X name (string) X rail-type (string) X section* (* señala que se pueden agregar 0 o más tramos)
 ;: REC: line (list)
 (define line
   (lambda (id name rail-type . sections)
     (list id name rail-type sections)))
-#|
-(define l0 (line 0 "Línea 0" "UIC 60 ASCE"))
-(define l1 (line 1 "Línea 1" "100 R.E." s0 s1 s2 s3 s5 s7 s8 s9))
-|#
 ;; Req 5: Funcion que permite determinar el largo total de una línea (en la unidad de medida expresada en cada tramo)
 ;; DOM: line (line)
 ;; REC: positive-number (int)
@@ -69,7 +36,20 @@
 ;; REC: positive-number (int)
 (define line-section-length
   (lambda (line station1-name station2-name)
-    (get-section-distance (first (filter (section-contains-stations? station1-name station2-name) (get-line-sections line))))))
+    (define fn-interna
+      (lambda (line-sections station1-name station2-name acc indicador)
+        (cond
+          [(null? line-sections) acc]
+          [(and (equal? (get-station-name (get-station1 (get-first-line-section line-sections))) station1-name)
+                (equal? (get-station-name (get-station2 (get-first-line-section line-sections))) station2-name))
+           (fn-interna (cdr line-sections) station1-name station2-name (+ acc (get-section-distance (get-first-line-section line-sections))) indicador)]
+          [(equal? (get-station-name (get-station1 (get-first-line-section line-sections))) station1-name)
+           (fn-interna (cdr line-sections) station1-name station2-name (+ acc (get-section-distance (get-first-line-section line-sections))) (+ indicador 1))]
+          [(and (equal? (get-station-name (get-station2 (get-first-line-section line-sections))) station2-name) (eq? indicador 1))
+           (fn-interna (cdr line-sections) station1-name station2-name (+ acc (get-section-distance (get-first-line-section line-sections))) (- indicador 1))]
+          [(eq? indicador 1) (fn-interna (cdr line-sections) station1-name station2-name (+ acc (get-section-distance (get-first-line-section line-sections))) indicador)]
+          [else (fn-interna (cdr line-sections) station1-name station2-name acc indicador)])))
+    (fn-interna (get-line-sections line) station1-name station2-name 0 0)))
 ;; Req 7: Función que permite determinar el costo total (monetario) de recorrer una línea. Resolver con recursividad natural.
 ;; DOM: line (line)
 ;; REC: positive-number U {0}
@@ -81,24 +61,25 @@
             0
             (+ (get-section-cost (first sections)) (intern-line-cost (cdr sections))))))
     (intern-line-cost (get-line-sections line))))
-;(line-cost l1)
-
 ;; Req 8: Función que permite determinar el costo de un trayecto entre una estación origen y una final. Resolver con recursividad de cola.
 ;; DOM: line (line) X station1-name (String) X station2-name (String)
 ;; REC: positive-number U {0}
 (define line-section-cost
   (lambda (line station1-name station2-name)
-    (define intern-line-section-cost
-      (lambda (sections station1-name station2-name cost)
+    (define fn-interna
+      (lambda (line-sections station1-name station2-name acc indicador)
         (cond
-          [(null? sections) cost]
-          [else
-           (if ((section-contains-stations? station1-name station2-name) (first sections))
-               (intern-line-section-cost (cdr sections) station1-name station2-name (+ cost (get-section-cost (first sections))))
-               (intern-line-section-cost (cdr sections) station1-name station2-name cost))])))
-    (intern-line-section-cost (get-line-sections line) station1-name station2-name 0)))
-;(line-section-cost l1 "USACH" "San Pablo")
-
+          [(null? line-sections) acc]
+          [(and (equal? (get-station-name (get-station1 (get-first-line-section line-sections))) station1-name)
+                (equal? (get-station-name (get-station2 (get-first-line-section line-sections))) station2-name))
+           (fn-interna (cdr line-sections) station1-name station2-name (+ acc (get-section-cost (get-first-line-section line-sections))) indicador)]
+          [(equal? (get-station-name (get-station1 (get-first-line-section line-sections))) station1-name)
+           (fn-interna (cdr line-sections) station1-name station2-name (+ acc (get-section-cost (get-first-line-section line-sections))) (+ indicador 1))]
+          [(and (equal? (get-station-name (get-station2 (get-first-line-section line-sections))) station2-name) (eq? indicador 1))
+           (fn-interna (cdr line-sections) station1-name station2-name (+ acc (get-section-cost (get-first-line-section line-sections))) (- indicador 1))]
+          [(eq? indicador 1) (fn-interna (cdr line-sections) station1-name station2-name (+ acc (get-section-cost (get-first-line-section line-sections))) indicador)]
+          [else (fn-interna (cdr line-sections) station1-name station2-name acc indicador)])))
+    (fn-interna (get-line-sections line) station1-name station2-name 0 0)))
 ;; Req 9: Función que permite añadir tramos a una línea
 ;; DOM: line (line) X section (section)
 ;; REC: line
@@ -118,13 +99,6 @@
           [else
            (cons (car sections) (intern-line-add-section (cdr sections) section))])))
     (intern-line-add-section (get-line-sections line) section)))
-
-#|
-(define e11 (station 12 "Baquedano" "t" 40))
-(define s10 (section e10 e11 5 20))
-|#
-
-;(line-add-section l1 s10)
 
 ;; Req 10: Función que permite determinar si un elemento cumple con las restricciones señaladas en apartados anteriores en
 ;; relación a las estaciones y tramos para poder conformar una línea. Ocupar un acumulador
@@ -265,3 +239,172 @@ t1
     (apply append sub (list line+))))
 
 ;; arreglar add-section a lineas, no esta funcionando bien
+
+
+
+;Estaciones L1 simplificada metro santiago
+(define e0 (station 0 "San Pablo" t 90))
+(define e1 (station 1 "Neptuno" r 45))
+(define e2 (station 2 "Pajaritos" c 45))
+(define e3 (station 3 "Las Rejas" r 45))
+(define e4 (station 4 "Ecuador" r 60))
+(define e5 (station 5 "San Alberto Hurtado" r 40))
+(define e6 (station 6 "Universidad de Santiago de Chile" c 40))
+(define e7 (station 7 "Estación Central" c 45))
+(define e8 (station 8 "Unión Latinoamericana" r 30))
+(define e9 (station 9 "República" r 40))
+(define e10 (station 10 "Los Héroes" c 60))
+(define e11 (station 11 "La Moneda" r 40))
+(define e12 (station 12 "Universidad de Chile" c 90))
+(define e13 (station 13 "Santa Lucía" r 40))
+(define e14 (station 14 "Universidad Católica" c 60))
+(define e15 (station 15 "Baquedano" r 40))
+(define e16 (station 16 "Los Dominicos" t 90))
+(define e17 (station 17 "Cochera Neptuno" m 3600))
+
+;Estaciones L2 simplificada metro santiago, para una versión circular
+(define e18 (station 18 "El Llano" r 60))
+(define e19 (station 19 "Franklin" r 50))
+(define e20 (station 20 "Rondizzoni" r 55))
+(define e21 (station 21 "Parque O'Higgins" r 65))
+(define e22 (station 22 "Toesca" r 65))
+(define e23 (station 23 "Santa Ana" c 65))
+(define e24 (station 24 "Puente Cal y Canto" r 65))
+
+;Tramos Línea 1
+(define s0 (section e0 e1 4 15))
+(define s1 (section e1 e2 3 14))
+(define s2 (section e2 e3 2.5 10))
+(define s3 (section e3 e4 4.5 17))
+(define s4 (section e4 e5 4.7 18))
+(define s5 (section e5 e6 4.3 17))
+(define s6 (section e6 e7 3.8 12))
+(define s7 (section e7 e8 2.5 10))
+(define s8 (section e8 e9 4.5 17))
+(define s9 (section e9 e10 4.7 18))
+(define s10 (section e10 e11 4.3 17))
+(define s11 (section  e11 e12 3.8 12))
+(define s12 (section e12 e13 4.5 17))
+(define s13 (section e13 e14 4.7 18))
+(define s14 (section e14 e15 4.3 17))
+(define s15 (section e15 e16 4.2 17))
+;enlace cochera
+(define s16 (section e1 e17 3.8 12))
+
+;Tramos Línea 2, línea circular
+(define s17 (section e18 e19 4 15))
+(define s18 (section e19 e20 3 12))
+(define s19 (section e20 e21 5 18))
+(define s20 (section e21 e22 4.5 16))
+(define s21 (section e22 e10 4.2 16))
+(define s22 (section e10 e23 4.2 16))
+(define s23 (section e23 e24 4.2 16))
+(define s24 (section e24 e18 28 90));excepcion
+
+;Creación de Línea 1 con todos los tramos
+(define l1 (line 1 "Línea 1" "UIC 60 ASCE" s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11 s12 s13 s14 s15))
+;Creación de Línea 2 sin incluir tramos
+(define l2 (line 2 "Línea 2" "100 R.E."))
+(line-length l1) ;resultado debe ser 64,3 si considera inclusive los tramos hacia estaciones de mantenimiento 
+(line-length l2) ;resultado debe ser 0
+l1
+;(line-cost l1) ;resultado debe ser 246 si considera inclusive los tramos hacia estaciones de mantenimiento 
+;(line-cost l2) ;resultado debe ser 0
+(line-section-length l1 "San Pablo" "Las Rejas")   ;respuesta es 9.5s
+(line-section-cost l1 "San Pablo" "Las Rejas")     ;respuesta es 39
+
+#|
+
+;añadiendo tramos a l2
+(define l2a (line-add-section l2 s17))
+(define l2b (line-add-section l2a s18))
+(define l2c (line-add-section l2b s19))
+(define l2d (line-add-section l2c s20))
+(define l2e (line-add-section l2d s21))
+(define l2f (line-add-section l2e s22))
+(define l2g (line-add-section l2f s23))
+(define l2h (line-add-section l2g s24))
+(define l2i (line-add-section l2h s19))  ;dependiendo de como implemente la función, esta operación no añade la estación duplicada. Puede lanzar un “error o excepción” (no un mensaje de error como String, para no comprometer el recorrido de la función) o bien devolver la línea de entrada intacta. En este caso, l2i sería igual a l2h. 
+
+(line? l1)  ;devuelve true
+(line? l2)  ;devuelve false
+(line? l2e)  ;devuelve false
+(line? l2h)  ;devuelve true
+
+;creando carros
+(define pc0 (pcar 0 100 "NS-74" tr))
+(define pc1 (pcar 1 100 "NS-74" ct))
+(define pc2 (pcar 2 150 "NS-74" ct))
+(define pc3 (pcar 3 100 "NS-74" ct))
+(define pc4 (pcar 4 100 "NS-74" tr))
+(define pc5 (pcar 5 100 "AS-2014" tr))
+(define pc6 (pcar 6 100 "AS-2014" ct))
+(define pc7 (pcar 7 100 "AS-2014" ct))
+(define pc8 (pcar 8 100 "AS-2014" ct))
+(define pc9 (pcar 9 100 "AS-2014" tr))
+(define pc10 (pcar 10 100 "AS-2014" tr))
+(define pc11a (pcar 11 100 "AS-2016" tr))
+(define pc11 (pcar 12 100 "AS-2016" ct))
+(define pc12 (pcar 13 100 "AS-2016" ct))
+(define pc13 (pcar 14 150 "AS-2016" ct))
+(define pc14 (pcar 15 100 "AS-2016" ct))
+(define pc15 (pcar 16 100 "AS-2016" ct))
+(define pc16 (pcar 17 100 "AS-2016" ct))
+(define pc17 (pcar 18 100 "AS-2016" tr))
+
+;creando trenes
+(define t0 (train 0 "CAF" "UIC 60 ASCE" 60 1.5)) ;tren sin carros definidos
+(define t1 (train 1 "CAF" "UIC 60 ASCE" 70  2 pc0 pc1 pc2 pc3 pc4)) ;tren válido
+(define t2 (train 2 "CAF" "100 R.E." 70  2 pc5 pc6 pc7 pc8 pc9)) ;tren válido
+(define t3 (train 3 "CAF" "100 R.E." 70  2 pc11a pc11 pc12 pc13 pc14 pc15 pc16 pc17)) ;tren válido
+(define t4 (train 4 "CAF" "100 R.E." 70  2 pc1 pc2 pc3)) ;tren inválido sin terminales en extremos
+(define t5 (train 5 "CAF" "100 R.E." 70  2 pc0 pc5 pc9 pc12 pc17))  ;tren inválido por incompatibilidad de carros
+
+(define t0a (train-add-car t0 pc5 0))
+(define t0b (train-add-car t0a pc6 1))
+(define t0c (train-add-car t0b pc7 2))
+(define t0d (train-add-car t0c pc8 3))
+(define t0e (train-add-car t0d pc9 4)) ;tren válido
+
+(define t1a (train-remove-car t1 0))
+(define t1b (train-remove-car t1 2))
+
+;verificación de válidez en la conformación de trenes
+(train? t0) ;debe arrojar #f
+(train? t1) ;debe arrojar #t
+(train? t2) ;debe arrojar #t
+(train? t3) ;debe arrojar #t
+(train? t4) ;debe arrojar #f
+(train? t0a) ;debe arrojar #f
+(train? t0b) ;debe arrojar #f
+(train? t0c) ;debe arrojar #f
+(train? t0d) ;debe arrojar #f
+(train? t0e) ;debe arrojar #t
+(train? t1a) ;debe arrojar #f
+(train? t1b) ;debe arrojar #t
+
+;determinar capacidad del tren
+(train-capacity t0) ;debe arrojar 0
+(train-capacity t1) ;debe arrojar 550
+
+
+;Creando drivers
+(define d0 (driver 0 "Juan" "CAF"))
+(define d1 (driver 1 "Alejandro" "Alsthom"))
+(define d2 (driver 2 "Diego" "Alsthom"))
+(define d3 (driver 3 "Pedro" "CAF"))
+
+;Creando Metros
+(define sw0 (subway 0 "Metro de Santiago"))
+(define sw1 (subway 1 "Subte"))
+
+;Agregando trenes
+(define sw0a (subway-add-train sw0 t1 t2 t0e))
+
+;Agregando lineas
+(define sw0b (subway-add-line sw0a l1 l2h))
+
+;Agregando drivers
+(define sw0c (subway-add-driver sw0b d0 d1 d2 d3))
+
+|#
